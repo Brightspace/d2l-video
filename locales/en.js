1595278051e95a4b610aaef4fbd60c24946cdee9
Evan Lemieux <Evan.Lemieux@D2L.com>
2019-10-31 14:41:06 -0500

    fixed what was making circleci sad

---
+++ locales/en.js
@@ -0,0 +1,20 @@
+/* eslint-disable quotes, indent */
+window.D2L = window.D2L || {};
+window.D2L.PolymerBehaviors = window.D2L.PolymerBehaviors || {};
+window.D2L.PolymerBehaviors.D2LVideo = window.D2L.PolymerBehaviors.D2LVideo || {};
+window.D2L.PolymerBehaviors.D2LVideo.LangTerms = window.D2L.PolymerBehaviors.D2LVideo.LangTerms || {};
+
+/**
+	* This file is auto-generated. Do not modify.
+	* @polymerBehavior
+	*/
+window.D2L.PolymerBehaviors.D2LVideo.LangTerms["en"] = {
+	"Play": "Play",
+	"Pause": "Pause",
+	"Volume": "Volume",
+	"VolumeLevel": "Volume {rawVolume}",
+	"Fullscreen": "Fullscreen",
+	"EvidenceVideoPlayer": "Evidence Video Player",
+	"VolumeBar": "Volume Bar",
+	"SeekBar": "Seek Bar"
+};
